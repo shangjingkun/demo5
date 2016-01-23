8fd5edd3c1c407f2cf46a76a1f956a83e4a24ade
shangjingkun <919029153@qq.com>
2016-01-23 13:05:24 +0800

    demo555

---
+++ demo55/demo55/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  demo55
+//
+//  Created by CC on 16/1/23.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"aaaddeeedd");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
